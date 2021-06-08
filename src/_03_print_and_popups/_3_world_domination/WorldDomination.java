394594bc4e51a17cacdb7c312c8b47dff695be93
student <student@ip-172-31-50-142.us-west-2.compute.internal>
2021-06-07 18:26:39 -0700

    I made a couple quizzes. Even though I made them I still could not pass them...

--- src/_03_print_and_popups/_3_world_domination/WorldDomination.java
+++ src/_03_print_and_popups/_3_world_domination/WorldDomination.java
@@ -6,9 +6,14 @@ import javax.swing.JOptionPane;
 public class WorldDomination {
 	
 	public static void main(String[] args) {
-		
-		// 1. Ask the user if they know how to write code.
 
+		// 1. Ask the user if they know how to write code.
+		String input = JOptionPane.showInputDialog("What is 7 + 1?");
+		if (input.equalsIgnoreCase("yes")) {
+			JOptionPane.showMessageDialog(null,  "8");
+		}else {
+			JOptionPane.showMessageDialog(null, "Sign up for classes in First Grade");
+		}
 		// 2. If they say "yes", tell them they will rule the world.
 
 		// 3. Otherwise, tell them to sign up for classes at The League.
--- src/_03_print_and_popups/_4_quiz_game/QuizGame.java
+++ src/_03_print_and_popups/_4_quiz_game/QuizGame.java
@@ -1,23 +1,66 @@
 package _03_print_and_popups._4_quiz_game;
 
+import javax.swing.JOptionPane;
+
 public class QuizGame {
 	public static void main(String[] args) {
 		
-		// Create a variable to hold the user's score. Set it equal to zero. 
-		
-		// ASK A QUESTION AND CHECK THE ANSWER
-		
-				// 2.  Ask the user a question 
+		int score = 0;
+		JOptionPane.showMessageDialog(null, "In order to win, have a final score of 3!");
+
+		String input = JOptionPane.showInputDialog("What is 7 + 1?");
+		if (input.equalsIgnoreCase("8")) {
+			JOptionPane.showMessageDialog(null, "Good Job!");
+			 score = score + 1;
+		}else {
+			JOptionPane.showMessageDialog(null, "Sign up for classes in First Grade");
+			 score = score - 1;
+			 
+		}
+		input = JOptionPane.showInputDialog("How long is an Olympic swimming pool? (In Meters)");
+		if (input.equalsIgnoreCase("50")) {
+			JOptionPane.showMessageDialog(null, "Good Job!");
+			 score = score + 1;
+		}else {
+			JOptionPane.showMessageDialog(null, "It was 50, Sorry.");
+			 score = score - 1;
+			 
+		}
+		input = JOptionPane.showInputDialog("What is (x-1)(2)?");
+		if (input.equalsIgnoreCase("2x-2")) {
+			JOptionPane.showMessageDialog(null, "Good Job!");
+			 score = score + 1;
+		}else {
+			JOptionPane.showMessageDialog(null, "Sign up for classes in Sixth Grade");
+			 score = score - 1;
+			 
+		}
+		input = JOptionPane.showInputDialog("What is cynophobia?");
+		if (input.equalsIgnoreCase("The Fear of Dogs")) {
+			JOptionPane.showMessageDialog(null, "Good Job!");
+			 score = score + 1;
+		}else {
+			JOptionPane.showMessageDialog(null, "It is the fear of dogs. ");
+			 score = score - 1;
+		}
+		input = JOptionPane.showInputDialog("How many languages are written from left to right?");
+		if (input.equalsIgnoreCase("12")) {
+			JOptionPane.showMessageDialog(null, "Good Job!");
+			 score = score + 1;
+		}else {
+			JOptionPane.showMessageDialog(null, "THe correct answer was 12 languages");
+			 score = score - 1;
+		}
+
 				
-				// 3.  Use an if statement to check if their answer is correct
 				
-				// 4.  if the user's answer was correct, add one to their score 
-		
-		// MAKE MORE QUESTIONS. Ask more questions by repeating the above 
-				// Option: Subtract a point from their score for a wrong answer
-		
-		
-		// After all the questions have been asked, tell the user their final score 
-		
+		JOptionPane.showMessageDialog(null, "Your final Score was...");
+		JOptionPane.showMessageDialog(null, score);
+		if (score == 3) {
+			JOptionPane.showMessageDialog(null, "You Won!!!");
+		}
+		if (score <= 3) {
+			JOptionPane.showMessageDialog(null, "You Lost :(");
+		}
 	}
 }
